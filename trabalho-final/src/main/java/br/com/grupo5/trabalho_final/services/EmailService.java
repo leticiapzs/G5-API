1bd798de7d3b7b0cfab472927b5ac3af9b60f35c
FFred-Fernandes <fredcostafernandes@gmail.com>
2024-10-09 20:48:54 -0300

    EmailService acertando

--- trabalho-final/src/main/java/br/com/grupo5/trabalho_final/services/EmailService.java
+++ trabalho-final/src/main/java/br/com/grupo5/trabalho_final/services/EmailService.java
@@ -10,14 +10,10 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
-//import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-
-//import jakarta.mail.MessagingException;
-//import jakarta.mail.internet.MimeMessage;
 
 @Component
 public class EmailService {
