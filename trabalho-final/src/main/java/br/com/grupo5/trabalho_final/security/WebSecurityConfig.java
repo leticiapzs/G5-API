db5bf6bd262e9b2dee5b615c070f5f89de7526c3
FFred-Fernandes <fredcostafernandes@gmail.com>
2024-10-22 17:51:18 -0300

    Endereco get e delete

--- trabalho-final/src/main/java/br/com/grupo5/trabalho_final/security/WebSecurityConfig.java
+++ trabalho-final/src/main/java/br/com/grupo5/trabalho_final/security/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/g6/**", "/auth/**", "/h2-console/**", "/roles/**", "/test/all/**",
 								"/swagger-ui/**", "/v3/api-docs/**", "/actuator/**", "user/**", "/cliente/**", "/produto/**",
-								"/endereco/**","/adicionando-endereco/**", "/pedido/**", "/loja/**")
+								"/endereco/**","/adicionando-endereco/**", "/buscando-endereco/{id}/**", "/deletando-endereco/{id}/**", "/pedido/**", "/loja/**")
 						.permitAll()
 						.requestMatchers("/test/user/**").hasAnyRole("USER", "ADMIN")
 						.requestMatchers("/test/admin/**").hasRole("ADMIN")
--- trabalho-final/src/main/java/br/com/grupo5/trabalho_final/security/controllers/EnderecoController.java
+++ trabalho-final/src/main/java/br/com/grupo5/trabalho_final/security/controllers/EnderecoController.java
@@ -1,13 +1,19 @@
 package br.com.grupo5.trabalho_final.security.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.grupo5.trabalho_final.security.dto.EnderecoRequestDTO;
 import br.com.grupo5.trabalho_final.security.dto.EnderecoResponseDTO;
 import br.com.grupo5.trabalho_final.security.services.EnderecoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/endereco")
@@ -25,5 +31,20 @@ public class EnderecoController {
   public EnderecoResponseDTO testeEndereco(@RequestBody EnderecoRequestDTO enderecoDto) {
 	  return enderecoService.consultarEndereco(enderecoDto);  
   }
+
+	@GetMapping("/buscando-endereco/{id}")
+	public EnderecoResponseDTO buscarEndereco(@PathVariable Integer id) {
+		return enderecoService.buscarEndereco(id);
+	}
+	
+	@DeleteMapping("/deletando-endereco/{id}")
+	public ResponseEntity<String> deletarId(@PathVariable Integer id) {
+		boolean resultDelete = enderecoService.enderecoDelete(id);
+		if(resultDelete) {
+			return ResponseEntity.status(HttpStatus.OK).body("Endereço deletado com sucesso!");
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Endereço não deletado!");
+		}
+	}
 
 }
