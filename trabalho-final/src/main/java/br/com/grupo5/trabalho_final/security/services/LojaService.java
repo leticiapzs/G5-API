ce2437c19c971bcc5a96b517aa5b240ec39f260e
Gabriel Toledo <gabrieltoledo4629@gmail.com>
2024-10-22 15:56:23 -0300

    Adicionando o Get e Get By Id

--- trabalho-final/src/main/java/br/com/grupo5/trabalho_final/security/services/LojaService.java
+++ trabalho-final/src/main/java/br/com/grupo5/trabalho_final/security/services/LojaService.java
@@ -1,8 +1,29 @@
 package br.com.grupo5.trabalho_final.security.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+
+import br.com.grupo5.trabalho_final.security.entities.Loja;
+import br.com.grupo5.trabalho_final.security.repositories.LojaRepository;
 
 @Service
 public class LojaService {
+	@Autowired
+	private LojaRepository lojaRepository;
+
+	public List<Loja> getAllLojas() {
+
+		return lojaRepository.findAll();
+
+	}
+
+	public Loja getLojaById(Integer id) {
+		Loja loja = lojaRepository.findById(id).get();
+		return loja;
+
+	}
 
 }
