9ca596037b277f22773deee82e18eb3c7bc83134
João Moreira <jgmoreira1809@gmail.com>
2024-10-23 09:45:29 -0300

    Categoria implemented
    Categoria oneToMany with Produto
    Version Update from 2.6.0 to 2.7.0

---
+++ trabalho-final/src/main/java/br/com/grupo5/trabalho_final/security/entities/Categoria.java
@@ -0,0 +1,66 @@
+package br.com.grupo5.trabalho_final.security.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Categoria {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "cat_cd_id")
+  private Integer id;
+
+  @Column(name = "cat_tx_nome")
+  private String nome;
+
+  @OneToMany
+  @JoinColumn(name = "cat_fk_prod")
+  private List<Produto> fkProduto;
+
+  public Categoria() {
+  }
+
+  public Categoria(Integer id, String nome, List<Produto> fkProduto) {
+    this.id = id;
+    this.nome = nome;
+    this.fkProduto = fkProduto;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Produto> getFkProduto() {
+    return fkProduto;
+  }
+
+  public void setFkProduto(List<Produto> fkProduto) {
+    this.fkProduto = fkProduto;
+  }
+
+  @Override
+  public String toString() {
+    return "Categoria [id=" + id + ", nome=" + nome + ", fkProduto=" + fkProduto + "]";
+  }
+
+}
